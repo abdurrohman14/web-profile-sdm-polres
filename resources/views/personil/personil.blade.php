d9cfe019fe7e1d0843f8abae47eb93df751bbe78
abdurrohman14 <rabd5428@gmail.com>
2024-10-03 10:57:40 +0700

    perubahan pada view role personil

--- resources/views/personil/personil.blade.php
+++ resources/views/personil/personil.blade.php
@@ -3,9 +3,10 @@
 
 <!-- Basic Card Example -->
 
-<div class="card shadow mb-4">
-    <div class="card-header py-3">
+<div class="card shadow mb-2">
+    <div class="card-header d-flex justify-content-between py-3">
         <h6 class="m-0 font-weight-bold text-dark">Informasi Data Diri</h6>
+        <h6 class="m-0 font-weight-bold text-dark">{{ \Carbon\Carbon::now()->locale('id')->translatedFormat('l, d F Y') }}</h6>
     </div>
     <div class="card-body">
         @if ($personel)
@@ -15,18 +16,38 @@
                     <img src="{{ asset('storage/personil/' . $personel->gambar) }}" alt="Foto Personel" style="width: 100%; max-width: 298px; height: auto; border-radius: 8px;">
                 </div>
                 <!-- Kolom Informasi -->
-                <div class="person-info">
-                    <h5>{{ $personel->nama_lengkap }}</h5>
+                <div class="person-info mt-5">
+                    <h5 class="font-weight-bold text-dark">{{ $personel->nama_lengkap }}</h5>
                     <!-- Informasi dengan Flexbox untuk rata titik dua -->
+                    <div class="info-item d-flex mb-2">
+                        <div class="label" style="min-width: 150px;"><strong>NRP</strong></div>
+                        <div class="colon">:</div>
+                        <div class="value ml-2">{{ $personel->nrp }}</div>
+                    </div>
                     <div class="info-item d-flex mb-2">
                         <div class="label" style="min-width: 150px;"><strong>Jabatan</strong></div>
                         <div class="colon">:</div>
-                        <div class="value ml-2">{{ $personel->jabatan->nama }}</div>
+                        <div class="value ml-2">{{ $personel->jabatan->nama }} / {{ $personel->subJabatan->nama }}</div>
                     </div>
                     <div class="info-item d-flex mb-2">
                         <div class="label" style="min-width: 150px;"><strong>Pangkat Polri</strong></div>
                         <div class="colon">:</div>
                         <div class="value ml-2">{{ $personel->pangkat->nama }}</div>
+                    </div>
+                    <div class="info-item d-flex mb-2">
+                        <div class="label" style="min-width: 150px;"><strong>Alamat</strong></div>
+                        <div class="colon">:</div>
+                        <div class="value ml-2">{{ $personel->alamat_personel }}</div>
+                    </div>
+                    <div class="info-item d-flex mb-2">
+                        <div class="label" style="min-width: 150px;"><strong>Suku</strong></div>
+                        <div class="colon">:</div>
+                        <div class="value ml-2">{{ $personel->suku }}</div>
+                    </div>
+                    <div class="info-item d-flex mb-2">
+                        <div class="label" style="min-width: 150px;"><strong>Status</strong></div>
+                        <div class="colon">:</div>
+                        <div class="value ml-2" style="font-weight:bold; color: {{ $personel->status == 'Aktif' ? 'green' : 'red' }}">{{ $personel->status }}</div>
                     </div>
                     {{-- <div class="info-item d-flex mb-2">
                         <div class="label" style="min-width: 150px;"><strong>Pangkat PNS Polri</strong></div>
